03f60e55cece7e92a665554e6fcbf02d0295ca62
mariaperez15 <123765659+mariaperez15@users.noreply.github.com>
2023-05-22 15:26:58 +0000

    cambiar luz

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -7,14 +7,14 @@ import rigoImage from "../../img/rigo-baby.jpg";
 //create your first component
 const Home = () => {
 
-	//const [selected, setSelected] = useState ("red");
-	let selected = "yellow";
+	const [selected, setSelected] = useState ("green");
+	//let selected = "yellow";
 
 	return (
 		<div className="semaforo">
-			<div className={"light red" + (selected === "red" ? "glow" : "")}></div>
-			<div className={"light yellow" + (selected === "yellow" ? "glow" : "")}></div>
-			<div className={"light green" + (selected === "green" ? "glow" : "")}></div>
+			<div onClick={() => {setSelected("red")}} className={"light red " + " " + (selected === "red" ? "glow" : "")}></div>
+			<div onClick={() => {setSelected("yellow")}} className={"light yellow " + " " + (selected === "yellow" ? "glow" : "")}></div>
+			<div onClick={() => {setSelected("green")}} className={"light green" + " " + (selected === "green" ? "glow" : "")}></div>
 		
 		</div>
 	);
